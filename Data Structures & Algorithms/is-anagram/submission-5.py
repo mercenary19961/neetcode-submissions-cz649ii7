77b8f096f13d3f26a4d9dd47f9ab11600ef91957
Zaid <sabbaghzaid88@gmail.com>
2026-05-28 11:38:03 +0300

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+            
+        hashS, hashT = {}, {}
+
+        for c in s:
+            hashS[c] = 1 + hashS.get(c, 0)
+        
+        for c in t:
+            hashT[c] = 1 + hashT.get(c, 0)
+
+        for key in hashS:
+            if hashS[key] != hashT.get(key, 0): return False
+
+        return True
